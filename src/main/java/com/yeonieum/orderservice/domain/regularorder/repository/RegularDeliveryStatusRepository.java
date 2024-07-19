9e82b71e68a8f3ea49b4961e7346d4e58ece7dd1
Koo-Tae-Ho <gth0908@naver.com>
2024-07-19 20:36:44 +0900

    KAN-439 init: 정기배송상태 영속계층 구축

---
+++ src/main/java/com/yeonieum/orderservice/domain/regularorder/repository/RegularDeliveryStatusRepository.java
@@ -0,0 +1,7 @@
+package com.yeonieum.orderservice.domain.regularorder.repository;
+
+import com.yeonieum.orderservice.domain.regularorder.entity.RegularDeliveryStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegularDeliveryStatusRepository extends JpaRepository<RegularDeliveryStatus, Long> {
+}
