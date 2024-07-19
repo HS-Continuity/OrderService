3b7f118fda5b0dc5e5a18c645ad2c9b741c5ab27
kimujae <dnwo0103@naver.com>
2024-07-20 00:23:01 +0900

    KAN-451 feat: 결제정보레포지토리 주문ID로 결제정보조회 쿼리메서드 정의

--- src/main/java/com/yeonieum/orderservice/domain/order/repository/PaymentInformationRepository.java
+++ src/main/java/com/yeonieum/orderservice/domain/order/repository/PaymentInformationRepository.java
@@ -4,4 +4,5 @@ import com.yeonieum.orderservice.domain.order.entity.PaymentInformation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentInformationRepository extends JpaRepository<PaymentInformation,Long> {
+    PaymentInformation findByOrderDetail_OrderDetailId(String orderDetailId);
 }
