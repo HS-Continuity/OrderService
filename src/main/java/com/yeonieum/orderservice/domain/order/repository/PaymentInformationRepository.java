73197fa02b562f9712174c76be7ab187217bde11
Koo-Tae-Ho <gth0908@naver.com>
2024-07-19 21:26:04 +0900

    KAN-446 init: 결제정보 영속계층 구축

---
+++ src/main/java/com/yeonieum/orderservice/domain/order/repository/PaymentInformationRepository.java
@@ -0,0 +1,7 @@
+package com.yeonieum.orderservice.domain.order.repository;
+
+import com.yeonieum.orderservice.domain.order.entity.PaymentInformation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentInformationRepository extends JpaRepository<PaymentInformation,Long> {
+}
