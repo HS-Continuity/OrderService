85c8474f6dcecf317bc4dfd6f1b691168d985979
kimujae <dnwo0103@naver.com>
2024-07-20 12:01:50 +0900

    KAN-461 feat: 정기주문-상품정보 조회 API 인터페이스 정의

--- src/main/java/com/yeonieum/orderservice/infrastructure/feignclient/ProductServiceFeignClient.java
+++ src/main/java/com/yeonieum/orderservice/infrastructure/feignclient/ProductServiceFeignClient.java
@@ -2,12 +2,25 @@ package com.yeonieum.orderservice.infrastructure.feignclient;
 
 import com.yeonieum.orderservice.domain.productstock.request.StockUsageRequest;
 import com.yeonieum.orderservice.domain.productstock.response.StockUsageResponse;
+import com.yeonieum.orderservice.domain.regularorder.dto.response.RegularOrderResponse;
+import com.yeonieum.orderservice.global.responses.ApiResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 
 @FeignClient(name = "productservice", url = "http://localhost:8020")
 public interface ProductServiceFeignClient {
     @PostMapping("/api/inventory/")
     StockUsageResponse.AvailableResponseList checkAvailableOrderProduct(@RequestBody StockUsageRequest.IncreaseStockUsageList increaseStockUsageDtoList);
+
+    @GetMapping("/api/management/{productId}")
+    ResponseEntity<ApiResponse<RegularOrderResponse.ProductOrder>> retrieveProductInformation(@PathVariable Long productId);
+
+    @GetMapping("/api/management/products")
+    ResponseEntity<ApiResponse<Map<Long, RegularOrderResponse.ProductOrder>>> bulkRetrieveProductInformation(@RequestParam List<Long> productIdList);
+
 }
