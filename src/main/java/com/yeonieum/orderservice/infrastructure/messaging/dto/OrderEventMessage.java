4ff1939b42976d27698e2ff06ad9d27f8e764f80
kimujae <dnwo0103@naver.com>
2024-07-30 15:01:14 +0900

    KAN-636 feat : 주문 및 정기주문 이벤트 메시지에 고객ID추가

--- src/main/java/com/yeonieum/orderservice/infrastructure/messaging/dto/OrderEventMessage.java
+++ src/main/java/com/yeonieum/orderservice/infrastructure/messaging/dto/OrderEventMessage.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @Builder
 public class OrderEventMessage {
     private String memberId;
+    private Long customerId;
     private String orderDetailId;
     private String eventType;
     private String topic;
