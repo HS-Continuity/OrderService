07ce5aa973b37e251f68fe033c99f6f6f393e350
김우재 <62458513+kimujae@users.noreply.github.com>
2024-08-01 03:20:07 +0900

    Eureka서버 구성정보 수정

--- src/main/java/com/yeonieum/orderservice/OrderserviceApplication.java
+++ src/main/java/com/yeonieum/orderservice/OrderserviceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
 @EnableFeignClients
