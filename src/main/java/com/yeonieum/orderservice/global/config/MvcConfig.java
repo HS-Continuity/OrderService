f260402f74abe52a98bae15c7396ff26fd8fd2af
kimujae <dnwo0103@naver.com>
2024-08-03 02:41:11 +0900

    KAN-701 feat: 배포 버그 수정

--- src/main/java/com/yeonieum/orderservice/global/config/MvcConfig.java
+++ src/main/java/com/yeonieum/orderservice/global/config/MvcConfig.java
@@ -7,17 +7,17 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class MvcConfig implements WebMvcConfigurer {
-    @Value("${cors.allowed.origin}")
-    private String CORS_ALLOWED_ORIGIN;
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5174", CORS_ALLOWED_ORIGIN)
-                .allowCredentials(true)
-                .allowedMethods("GET", "POST", "DELETE", "OPTIONS", "PATCH", "PUT")
-                .allowedHeaders("Content-Type", "application/json", "Authorization", "Bearer")
-                .maxAge(3600);
-    }
+//    @Value("${cors.allowed.origin}")
+//    private String CORS_ALLOWED_ORIGIN;
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:5173", "http://localhost:5174", CORS_ALLOWED_ORIGIN)
+//                .allowCredentials(true)
+//                .allowedMethods("GET", "POST", "DELETE", "OPTIONS", "PATCH", "PUT")
+//                .allowedHeaders("Content-Type", "application/json", "Authorization", "Bearer")
+//                .maxAge(3600);
+//    }
 }
