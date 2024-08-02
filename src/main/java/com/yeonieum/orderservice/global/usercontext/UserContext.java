e5bfdb04bb63ee697c121acc91736c72b34f4536
kimujae <dnwo0103@naver.com>
2024-08-02 15:05:52 +0900

    KAN-583 feat : UserContext에 롤타입과 UniqueId 추가

--- src/main/java/com/yeonieum/orderservice/global/usercontext/UserContext.java
+++ src/main/java/com/yeonieum/orderservice/global/usercontext/UserContext.java
@@ -18,6 +18,8 @@ public class UserContext {
     public static final String TRANSACTION_ID = "transaction-id";
     public static final String AUTH_TOKEN = "auth-token";
     public static final String USER_ID = "user-id";
+    public static final String UNIQUE_ID = "unique-id";
+    public static final String ROLE_TYPE = "role-type";
     public static final String SERVICE_ID = "service-id";
 
     @Builder.Default
@@ -28,4 +30,9 @@ public class UserContext {
     private String userId = new String();
     @Builder.Default
     private String serviceId = new String();
+    @Builder.Default
+    private String uniqueId = new String();
+    @Builder.Default
+    private String roleType = new String();
+
 }
