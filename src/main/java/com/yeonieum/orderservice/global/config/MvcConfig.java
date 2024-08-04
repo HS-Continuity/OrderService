19d533c69052ffe35f974b51c218ba9eafdac3cc
kimujae <dnwo0103@naver.com>
2024-08-04 22:32:03 +0900

    KAN-701 feat: cors 설정 도메인 추가

--- src/main/java/com/yeonieum/orderservice/global/config/MvcConfig.java
+++ src/main/java/com/yeonieum/orderservice/global/config/MvcConfig.java
@@ -11,10 +11,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     @Value("${cors.allowed.origin}")
     private String CORS_ALLOWED_ORIGIN;
+    @Value("${cors.allowed.origin.yeonieum}")
+    private String CORS_ALLOWED_ORIGIN_YEONIEUM;
+    @Value("${cors.allowed.origin.dashboard}")
+    private String CORS_ALLOWED_ORIGIN_DASHBOARD;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5174", CORS_ALLOWED_ORIGIN)
+                .allowedOrigins("http://localhost:5173", "http://localhost:5174", CORS_ALLOWED_ORIGIN, CORS_ALLOWED_ORIGIN_DASHBOARD, CORS_ALLOWED_ORIGIN_YEONIEUM)
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "DELETE", "OPTIONS", "PATCH", "PUT")
                 .allowedHeaders("Content-Type", "application/json", "Authorization", "Bearer")
